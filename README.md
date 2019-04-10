19440f4bb70b5e4e34d79b4d905c5c41c7d5459b
Dylan Davidson <rogue1037@gmail.com>
2019-04-09 22:51:46 -0700

    added to the readme and to the liri.js

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # liri-node-app
+
+**Creator**: `Dylan Davidson`
+
+**Created on**: `April 9th 2019`
+
+- - -
+
+## ABOUT THE APP
+LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives back data. The user has the option of using four commands (listed below) in conjuntion with specific parameters associated with the commands. The  `Commands` are:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+
+- - -
+## HOW TO USE LIRI
+### **Video Guide**
+
+Video link of me running through the functions.
+
+
+### **Step by Step instructions**
+
+1. Open your terminal such as Bash.
+2. Navigate to the folder that contains the `liri.js` file. 
+3. Depending on the command you run, the output will vary. 
+
+    **Example 1**: Run the `concert-this` command
+    
+        node liri.js concert-this <name of artist or band>
+    
+    Output: The system will display a list of all events and locations where the artist or band will perform. I haven't finished the moment function to fix the date. this will also append to the random.txt file.
+
+     **Example 2**: Run the `spotify-this-song` command
+    
+        node liri.js spotify-this-song <name of song>
+    
+    Output: The system will display a list of information associated with the song. I wasn't able to get the info out of the API. It is pseudocoded and I know I am close. I am still working on it now, so I may get it fixed before you get to it.
+
+    
+
+    **Example 3**: Run the `movie-this` command
+    
+        node liri.js movie-this <name of movie>
+    
+    Output: The system will display information associated with the movie. The system will also log all the results in the random.txt file. I also cannot get the undeifined to give the info for Mr. Nobody. It is coded, but I think I have it in the wrong spot.
+
+    **Example 4**: Run the `do-what-it-says` command
+        
+        node liri.js do-what-it-says
+        
+    Output: The system will read the text in the random.txt file, and perform the comman listed in the random.txt file. ** I am still trying to get the info from the random.txt file. It is another thing I am still working on.
+- - -
+
+## TECHNOLOGIES USED
+* Javascript
+* Nodejs
+* Node packages:
+    * Node-Spotify-API
+    * Request
+    * DotEnv
+* APIs used:
+    * Bands in Town
+    * OMDB
+    * Axios
+* Git
+* GitHub
--- liri.js
+++ liri.js
@@ -1,25 +1,35 @@
-//require("dotenv").config();
+require("dotenv").config();
 var axios = require("axios");
 //var keys = require("./keys.js");
-
-
+var fs = require('fs');
+//var Spotify = require('node-spotify-api');
 //var spotify = new Spotify(keys.spotify);
-
-var userInput = process.argv.slice(2).join(" ");
-
-// axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp&date=all").then(
-//   function(response) {
-
-//     console.log(response);
-//     console.log(JSON.stringify(data[0], null, 2));
-
-//   }
-// );
+var moment = require('moment');
 
 
+var action = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
+
+function bandsInTown() {
+  axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp&date=upcoming").then(
+    function (response) {
+      for (var i = 0; i < 5; i++) {
+        console.log("The venue: " + response.data[i].venue.name);
+        console.log("\n");
+        console.log("the city for the venue is " + response.data[i].venue.city);
+        console.log("\n");
+        console.log(response.data[i].datetime);
+        console.log("\n");
+
+      }
+    }
+  );
+}
+
+function OMDBmovie(){
 axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
   function (response) {
-    console.log(response.data.Ratings);
+    //console.log(response.data.Ratings);
     console.log("\n");
     console.log("The Title of the movie is " + response.data.Title);
     console.log("\n");
@@ -27,7 +37,11 @@ axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=tril
     console.log("\n");
     console.log("This movie was rated on imdb, a " + response.data.imdbRating);
     console.log("\n");
-    //console.log("Rotten Tomatoes score for this movie is " + response.data.Ratings[{ Source: "Rotten Tomatoes", Value: "",}]);
+    if (response.data.Ratings && response.data.Ratings[1] && response.data.Ratings[1].Source === 'Rotten Tomatoes') {
+      console.log('Rotten Tomatoes rating: ' + response.data.Ratings[1].Value);
+    } else {
+      console.log('Rotten Tomatoes rating does not exist for some reason. Don\'t ask me why!');
+    }
     console.log("\n");
     console.log("This movie was produced in the good old " + response.data.Country);
     console.log("\n");
@@ -37,11 +51,77 @@ axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=tril
     console.log("\n");
     console.log("The movie is about " + response.data.Plot);
     console.log("\n");
+  }
+);
+}
+ 
 
-  });
 
-if (userInput === undefined) {
-  axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then(function (response) {
-    console.log(response.data);
+//  if (userInput === undefined) {
+// //   axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then(function (response) {
+// //     console.log(response.data);
+// //   });
+// }
+
+
+function spotifySong() {
+  spotify.search({ type: "track", query: userInput }, function (err, data) {
+    if (err) {
+      return console.log("error accured: " + err);
+    }
+
+    console.log("artist " + data.tracks.items[0].artist[0].name);
+
+  }
+  );
+}
+
+function getRandom() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data);
+    var dataArr = data.split(",");
+    console.log(dataArr);
+
   });
 }
+
+fs.appendFile("random.txt", ", " + action + " " + userInput, function (err) {
+
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("added to random.txt");
+  }
+
+});
+
+
+switch (action) {
+
+  case "concert-this":
+    bandsInTown();
+    break;
+
+  case "spotify-this-song":
+    spotifySong();
+    break;
+
+  case "movie-this":
+    OMDBmovie();
+    break;
+
+  case "do-what-it-says":
+    getRandom();
+    break;
+
+  default:
+    console.log('Invalid input. Accepted inputs are: concert-this <band here>, spotify-this-song <song here>, movie-this <movie here>, or do-what-it-says');
+    break;
+
+}
+
+
